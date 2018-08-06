ae13849ecb7355c8e9a2d999ace439edaa64797a
Silvio Amoroso - External <silvio.amoroso.external@yoox.com>
2018-08-06 17:48:20 +0200

    Inizio creazione classe casella

--- Campo_Minato.py
+++ Campo_Minato.py
@@ -1,3 +1,9 @@
+class casella:
+
+    def __init__(self, posizione):
+        self.posizione = posizione
+
+primo_punto = casella(13)
 
 w, h = 8, 5;
 campo = [[0 for x in range(w)] for y in range(h)]
@@ -5,11 +11,12 @@ campo = [[0 for x in range(w)] for y in range(h)]
 alfabeto = ("A", "B", "C", "D", "E")
 
 campo [0] = alfabeto
+campo [1] = (1, primo_punto.posizione, "?", "?", "?")
 
 #for m in range(len(alfabeto)):
 #    campo [m] = alfabeto [m]
 
 
-print(campo)
-print(' '+'\n'.join([''.join(['{:}'.format(item) for item in row])
+
+print(' '+'\n'.join([''.join(['{:4}'.format(item) for item in row])
       for row in campo]))
