94dd8c48ea0fc837ef1db2b6bc69bb059a90f0ff
Silvio Amoroso <silvioamoroso19@gmail.com>
2018-08-09 01:25:54 +0200

    Pulito tutto, manca parte degli zeri

--- Campo_Minato.py
+++ Campo_Minato.py
@@ -1,22 +1,114 @@
-class casella:
+import random
+import math
 
-    def __init__(self, posizione):
-        self.posizione = posizione
+class Casella:
+    caselle_uscite = []
+    posizione_bombe = []
 
-primo_punto = casella(13)
+    def __init__(self, x=0, y=0, bombeVicine=0):
+        self.x = x
+        self.y = y
+        self.bombeVicine = bombeVicine
 
-w, h = 8, 5;
-campo = [[0 for x in range(w)] for y in range(h)]
+    #Caselle già uscite
+    def casella_selezionata(self):
+        Casella.caselle_uscite.append((self.x, self.y))
 
-alfabeto = ("A", "B", "C", "D", "E")
+    def check_casella_uscita(self):
+        return (self.x, self.y) in Casella.caselle_uscite
 
-campo [0] = alfabeto
-campo [1] = (1, primo_punto.posizione, "?", "?", "?")
+    #Check se ho colpito la bomba
+    def check_bomba(self):
+        return (self.x, self.y) in Casella.posizione_bombe
 
-#for m in range(len(alfabeto)):
-#    campo [m] = alfabeto [m]
+    #La casella è limitrofa?
+    def caselle_vicine(self,other):
+        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2) <= math.sqrt(2) and math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2) != 0
+
+    #Numero di bombe vicine
+    def check_bombeVicine(self):
+        for bomba_prova in range(bombe):
+            bomba = Casella()
+            bomba.x = Casella.posizione_bombe[bomba_prova][0]
+            bomba.y = Casella.posizione_bombe[bomba_prova][1]
+            if self.caselle_vicine(bomba):
+                self.bombeVicine += 1
+        campo[int(self.y)][self.x] = self.bombeVicine
 
 
 
-print(' '+'\n'.join([''.join(['{:4}'.format(item) for item in row])
-      for row in campo]))
+# Creazione griglia
+xmax, ymax = 8, 5;
+numero_caselle = (xmax-1)*(ymax-1)
+campo = [["?" for x in range(xmax)] for y in range(ymax)]
+alfabeto = (" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "L")
+campo [0] = alfabeto[:xmax]
+for i in range(ymax-1):
+    campo[i+1][0]=i+1
+def printgriglia(griglia):
+    print('\n'.join([' '.join(['{}'.format(item) for item in row])
+          for row in campo]))
+printgriglia(campo)
+
+# Funzione creazione classe (per non ripeterla 10k volta
+def crea_casella(x, y):
+    casella = Casella()
+    casella.x = alfabeto.index(scelta[0])
+    casella.y = int(scelta[1])
+    return casella
+
+#Conta caselle ancora da apire: in input gli passo il campo da gioco
+def caselle_rimanenti(griglia):
+    global numero_caselle
+    numero_caselle = 0
+    for numeri in griglia:
+       for numero in numeri:
+           if numero == "?":
+               numero_caselle += 1
+    return(numero_caselle)
+
+# Crazione bombe
+bombe = 3
+bombe_create = 0
+def creazione_bombe():
+    global bombe_create
+    while bombe_create != bombe:
+        bomba = Casella()
+        bomba.x = random.randint(1,xmax-1)
+        bomba.y = random.randint(1,ymax-1)
+        if ((bomba.x, bomba.y) not in Casella.posizione_bombe) and ((bomba.x, bomba.y) not in Casella.caselle_uscite):
+            Casella.posizione_bombe.append((bomba.x, bomba.y))
+            bombe_create += 1
+
+#Dopo la prima scelta
+def primogiro():
+    global numero_caselle
+    numero_caselle -= 1
+    casella = crea_casella(alfabeto.index(scelta[0]), int(scelta[1]))
+    casella.casella_selezionata()
+    creazione_bombe()
+    casella.check_bombeVicine()
+    printgriglia(campo)
+    print(Casella.posizione_bombe)
+
+#Inizio gioco
+while True:
+    scelta = input('Inserisci le coordinate della casella --> ')
+    numero_caselle = caselle_rimanenti(campo)
+    if (scelta[0] not in alfabeto[1:xmax]) or (int(scelta[1]) not in range(ymax)):
+        print("Scegli dei valori corretti")
+    elif numero_caselle == (xmax-1)*(ymax-1):
+        primogiro()
+    else:
+        casella = crea_casella(alfabeto.index(scelta[0]), int(scelta[1]))
+        if casella.check_casella_uscita():
+            print ("Hai già scelto questa casella, per favore prova di nuovo!")
+        elif casella.check_bomba():
+            print("Vabbe ma allora sei un COGLIONE! Hai perso!!!")
+        elif (scelta[0] in alfabeto[1:xmax-1]) and (int(scelta[1]) in range(ymax-1)):
+            casella.casella_selezionata()
+            casella.check_bombeVicine()
+
+            printgriglia(campo)
+        else:
+            print("Scegli dei valori corretti")
